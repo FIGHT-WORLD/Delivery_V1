6a868fe6794329d655a86da6d7f2bddf3f1dbd6b
wonowonow <wonow_@naver.com>
2024-08-23 18:26:39 +0900

    design: User 생성 디자인

--- src/main/java/com/fight_world/mono/domain/user/model/User.java
+++ src/main/java/com/fight_world/mono/domain/user/model/User.java
@@ -1,13 +1,20 @@
 package com.fight_world.mono.domain.user.model;
 
+import com.fight_world.mono.domain.user.dto.request.UserSignUpDto;
 import com.fight_world.mono.domain.user.model.value_object.UserEmail;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
@@ -25,5 +32,23 @@ public class User {
 
     private String nickname;
 
+    @Builder(access = AccessLevel.PRIVATE)
+    public User(String username, String password, UserEmail email, String role, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.nickname = nickname;
+    }
 
+    public static User of(UserSignUpDto userSignUpDto, String encodedPassword) {
+
+        return User.builder()
+                   .username(userSignUpDto.username())
+                   .password(encodedPassword)
+                   .email(new UserEmail(userSignUpDto.email()))
+                   .role(userSignUpDto.role())
+                   .nickname(userSignUpDto.nickname())
+                   .build();
+    }
 }
