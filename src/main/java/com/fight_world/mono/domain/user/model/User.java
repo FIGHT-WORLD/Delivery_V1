89b09e5fe712086a9a3516dfd469320a27310467
Yoosup Shin <brian96shin@gmail.com>
2024-08-27 18:21:56 +0900

    Feat: UserEntity 변경 완료

--- src/main/java/com/fight_world/mono/domain/user/model/User.java
+++ src/main/java/com/fight_world/mono/domain/user/model/User.java
@@ -2,35 +2,66 @@ package com.fight_world.mono.domain.user.model;
 
 import com.fight_world.mono.domain.user.dto.request.UserSignUpDto;
 import com.fight_world.mono.domain.user.model.value_object.UserEmail;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-@Entity
+@Entity(name = "p_user")
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
+    @Column(nullable = false)
     private String password;
 
     @Embedded
     private UserEmail email;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserRole role;
 
+    @Column(nullable = false)
     private String nickname;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDateTime deletedAt;
 
     @Builder(access = AccessLevel.PRIVATE)
     public User(String username, String password, UserEmail email, UserRole role, String nickname) {
@@ -47,11 +78,12 @@ public class User {
     public static User of(UserSignUpDto userSignUpDto, String encodedPassword) {
 
         return User.builder()
-                   .username(userSignUpDto.getUsername())
-                   .password(encodedPassword)
-                   .email(new UserEmail(userSignUpDto.getEmail()))
-                   .role(UserRole.valueOf(userSignUpDto.getRole()))
-                   .nickname(userSignUpDto.getNickname())
-                   .build();
+                .username(userSignUpDto.getUsername())
+                .password(encodedPassword)
+                .email(new UserEmail(userSignUpDto.getEmail()))
+                .role(UserRole.valueOf(userSignUpDto.getRole()))
+                .nickname(userSignUpDto.getNickname())
+                .build();
     }
 }
+
