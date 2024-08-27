bd859e2f2c13e0328ee907956abf705b2776a029
Yoosup Shin <brian96shin@gmail.com>
2024-08-27 10:47:18 +0900

    Rename: SignUpUserRequestDto -> UserSignUpDto로 변경

--- src/main/java/com/fight_world/mono/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/fight_world/mono/domain/user/service/UserServiceImpl.java
@@ -15,7 +15,7 @@ public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
 
     @Override
-    public SignUpUserResponseDto signUpUser(SignUpUserRequestDto req) {
+    public UserSignUpDto signUpUser(UserSignUpDto req) {
         return null;
     }
 
