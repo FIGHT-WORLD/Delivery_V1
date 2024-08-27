f3e5adb596365affa3d900e5c20bf75c1905ae28
lsj104 <lsuj104@naver.com>
2024-08-27 11:42:10 +0900

    feat/report: 신고 작성

--- src/main/java/com/fight_world/mono/domain/report/message/ExceptionMessage.java
+++ src/main/java/com/fight_world/mono/domain/report/message/ExceptionMessage.java
@@ -9,8 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionMessage {
 
     REPORT_TITLE_VALID_MIN(HttpStatus.BAD_REQUEST, "제목은 최소 5자 이상 작성해 주셔야 합니다."),
-    REPORT_CONTENT_VALID_MIN(HttpStatus.BAD_REQUEST, "내용은 최소 10자 이상 작성해 주셔야 합니다."),
-    REPORT_TYPE_VALID(HttpStatus.BAD_REQUEST, "신고 유형을 선택해 주세요");
+    REPORT_CONTENT_VALID_MIN(HttpStatus.BAD_REQUEST, "내용은 최소 10자 이상 작성해 주셔야 합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
