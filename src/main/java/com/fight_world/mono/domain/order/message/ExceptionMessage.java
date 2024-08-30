153ba5ffd03522dcc09660fad8b52d930dff5e0a
wonowonow <wonow_@naver.com>
2024-08-30 11:31:47 +0900

    feat: OrderDelete 5분 제한 추가

--- src/main/java/com/fight_world/mono/domain/order/message/ExceptionMessage.java
+++ src/main/java/com/fight_world/mono/domain/order/message/ExceptionMessage.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ExceptionMessage {
 
     NOT_FOUND_ORDER(HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
-    ORDER_USER_VALID(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.");
+    ORDER_USER_VALID(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    CANNOT_DELETE_AFTER_TIMEOUT(HttpStatus.BAD_REQUEST, "5분이 지난 주문은 삭제할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
