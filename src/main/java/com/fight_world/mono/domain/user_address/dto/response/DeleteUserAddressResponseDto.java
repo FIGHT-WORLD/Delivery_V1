8e7a8cd46c0eb9d431e9403c3a132c5ad83167e5
Yoosup Shin <brian96shin@gmail.com>
2024-09-02 01:51:45 +0900

    Feat: DeleteUserAddressResponseDto 생성

--- src/main/java/com/fight_world/mono/domain/user_address/dto/response/DeleteUserAddressResponseDto.java
+++ src/main/java/com/fight_world/mono/domain/user_address/dto/response/DeleteUserAddressResponseDto.java
@@ -1,5 +1,18 @@
 package com.fight_world.mono.domain.user_address.dto.response;
 
-public record DeleteUserAddressResponseDto() {
+import com.fight_world.mono.domain.user_address.model.UserAddress;
+import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
+public record DeleteUserAddressResponseDto(
+        LocalDateTime deletedAt
+) {
+
+    public static DeleteUserAddressResponseDto from(UserAddress userAddress) {
+
+        return DeleteUserAddressResponseDto.builder()
+                .deletedAt(userAddress.getDeletedAt() == null ? LocalDateTime.MIN : userAddress.getDeletedAt())
+                .build();
+    }
 }
