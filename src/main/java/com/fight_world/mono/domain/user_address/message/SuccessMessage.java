26cd52a86baaa4d75ca9ce595ddbc9673a3ff11e
Yoosup Shin <brian96shin@gmail.com>
2024-09-02 01:52:21 +0900

    Feat: ExceptionMessage 추가

--- src/main/java/com/fight_world/mono/domain/user_address/message/SuccessMessage.java
+++ src/main/java/com/fight_world/mono/domain/user_address/message/SuccessMessage.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
     CREATE_SUCCESS_USER_ADDRESS(HttpStatus.CREATED, "주소 등록 성공"),
-    GET_SUCCESS_USER_ADDRESS_LIST(HttpStatus.CREATED, "주소 전체 조회 성공");
+    GET_SUCCESS_USER_ADDRESS_LIST(HttpStatus.OK, "주소 전체 조회 성공"),
+    DELETE_SUCCESS_USER_ADDRESS(HttpStatus.OK, "주소 삭제 성공"),
+    GET_SUCCESS_USER_ADDRESS(HttpStatus.OK, "주소 상세 조회 성공"),
+    UPDATE_SUCCESS_USER_ADDRESS(HttpStatus.OK, "주소 업데이트 성공");
 
     private final HttpStatus httpStatus;
     private final String message;
