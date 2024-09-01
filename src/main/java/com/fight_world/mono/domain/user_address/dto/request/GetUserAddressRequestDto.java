35417337ac9219b794656c8cd2eadc93dd3268ed
Yoosup Shin <brian96shin@gmail.com>
2024-09-02 01:52:01 +0900

    Feat: Dto 값 수정

---
+++ src/main/java/com/fight_world/mono/domain/user_address/dto/request/GetUserAddressRequestDto.java
@@ -0,0 +1,10 @@
+package com.fight_world.mono.domain.user_address.dto.request;
+
+import lombok.Builder;
+
+@Builder
+public record GetUserAddressRequestDto(
+
+) {
+
+}
--- src/main/java/com/fight_world/mono/domain/user_address/dto/response/GetUserAddressResponseDto.java
+++ src/main/java/com/fight_world/mono/domain/user_address/dto/response/GetUserAddressResponseDto.java
@@ -5,25 +5,35 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
-public record UpdateUserAddressResponseDto(
+public record GetUserAddressResponseDto(
+
         Long userId,
         String userAddressId,
         String address,
         String detailAddress,
         String request,
+        LocalDateTime createdAt,
+        Long createdBy,
         LocalDateTime updatedAt,
-        Long updatedBy
+        Long updatedBy,
+        LocalDateTime deletedAt,
+        Long deletedBy
 ) {
-    public static UpdateUserAddressResponseDto from(UserAddress userAddress) {
+    public static GetUserAddressResponseDto from(UserAddress userAddress) {
 
-        return UpdateUserAddressResponseDto.builder()
+        return GetUserAddressResponseDto.builder()
                 .userId(userAddress.getUser().getId())
                 .userAddressId(userAddress.getId())
                 .address(userAddress.getAddress())
                 .detailAddress(userAddress.getDetailAddress())
                 .request(userAddress.getRequest() == null ? "" : userAddress.getRequest())
+                .createdAt(userAddress.getCreatedAt())
+                .createdBy(userAddress.getCreatedBy())
                 .updatedAt(userAddress.getUpdatedAt())
                 .updatedBy(userAddress.getUpdatedBy())
+                .deletedAt(userAddress.getDeletedAt() == null ? LocalDateTime.MIN : userAddress.getDeletedAt())
+                .deletedBy(userAddress.getDeletedBy() == null ? Long.MIN_VALUE : userAddress.getDeletedBy())
                 .build();
     }
+
 }
