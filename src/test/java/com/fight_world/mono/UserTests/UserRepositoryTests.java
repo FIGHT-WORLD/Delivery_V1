f42ce3010d9ee2ace17b4ae9ff6ae81bc65e4ae7
wonowonow <wonow_@naver.com>
2024-09-02 23:51:44 +0900

    fix: activeProfiles 추가

--- src/test/java/com/fight_world/mono/UserTests/UserRepositoryTests.java
+++ src/test/java/com/fight_world/mono/UserTests/UserRepositoryTests.java
@@ -11,8 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class UserRepositoryTests {
 
     @Autowired
