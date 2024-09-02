cd5860c3272f78efc7f6747bdf398985af6a29ed
wonowonow <wonow_@naver.com>
2024-09-02 23:49:51 +0900

    fix: ci 때 테스트 오류 왜 날까용

--- src/test/java/com/fight_world/mono/UserTests/UserRepositoryTests.java
+++ src/test/java/com/fight_world/mono/UserTests/UserRepositoryTests.java
@@ -8,23 +8,26 @@ import com.fight_world.mono.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @DataJpaTest
 public class UserRepositoryTests {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    UserRepositoryTests(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
 
     @BeforeEach
     void beforeEach() {
         userRepository.deleteAll();
     }
 
-    @DisplayName("user의 static 메서드를 사용해 저장(패스워드 encode되지 않음)")
+    @DisplayName("user의 static 메서드를 사용해 저장(패스워드 encode됨)")
     @Test
     void test1() throws Exception {
         //given
@@ -35,10 +38,15 @@ public class UserRepositoryTests {
                 "user",
                 "zxcv1234");
 
-        User user = User.of(req, req.password());
+        String encodedPassword = passwordEncoder.encode(req.password());
+        User user = User.of(req, encodedPassword);
+
         //when
         userRepository.saveAndFlush(user);
+
         //then
-        assertThat(userRepository.findByUsername("user1234").get()).isEqualTo(user);
+        User foundUser = userRepository.findByUsername("user1234").get();
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser.getPassword()).isEqualTo(encodedPassword);
     }
 }
